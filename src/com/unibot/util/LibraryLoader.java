0d573be3cbe46afa9fa5946f3489bf462d89ee85
grandtix <grandtix@minitixpc>
2018-11-17 22:46:47 +0100

    version 1.0

--- src/com/unibot/util/LibraryLoader.java
+++ src/com/unibot/util/LibraryLoader.java
@@ -667,6 +667,7 @@ public class LibraryLoader {
 		_line = _line.replace("uint8_t *", "int ");
 		_line = _line.replace("  ", " ");
 	_line=_line.replace("&","");
+	
 		//System.out.println("apres: "+_line);
 		return _line;
 
